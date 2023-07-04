019b5e800d6054b35b4e03ab8667348f3bc43e87
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 16:39:48 +0800

    create three constants

---
+++ Fan/Fan_Class.py
@@ -0,0 +1,9 @@
+# Paulean Marguerette F. Parrish
+# BSCPE 1-4
+# (The Fan class).  Design a class named Fan to represent a fan. The class contains:
+
+# Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
