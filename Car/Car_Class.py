939a4e28031e2a670b1b65ec31c982191401d489
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 18:11:04 +0800

    create class for car and define attributes

---
+++ Car/Car_Class.py
@@ -0,0 +1,13 @@
+# Paulean Marguerette F. Parrish
+# BSCPE 1-4
+# Write a class named Car that has the following data attributes:
+class Car:
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+        # _ _year_model (for the car’s year model)
+        # _ _make (for the make of the car)
+        # _ _speed (for the car’s current speed)
+
