590f23645eff6f850b4a27d2804ab207eaee579f
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 16:46:50 +0800

    define constructor

--- Fan/Fan_Class.py
+++ Fan/Fan_Class.py
@@ -8,7 +8,13 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-    # A private int data field named speed that specifies the speed of the fan.
+    def __init__(self, speed, radius, color, on):
+        self.__speed = speed
+        self.__on = on
+        self.__radius = radius
+        self.__color = color
+
+# A private int data field named speed that specifies the speed of the fan.
 # A private bool data field named on that specifies whether the fan is on (the default is False).
 # A private float data field named radius that specifies the radius of the fan.
 # A private string data field named color that specifies the color of the fan.
