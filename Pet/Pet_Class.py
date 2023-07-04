21ce47137f9c055f415393ebe6aff1e590c1aa83
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 19:34:05 +0800

    methods for assigning and return

--- Pet/Pet_Class.py
+++ Pet/Pet_Class.py
@@ -9,3 +9,30 @@ class Pet:
         self.__name = ""
         self.__animal_type = ""
         self.__age = ""
+
+    # The Pet class should have an _ _init_ _ method that creates these attributes. It should also have the following methods:
+    # This method assigns a value to the _ _name field.
+    def set_name(self, name):
+        self.__name = name
+
+    # This method assigns a value to the _ _animal_type field.
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    # This method assigns a value to the _ _age field.  
+    def set_age(self, age):
+        self.__age = age
+
+    # This method returns the value of the _ _ name field.
+    def get_name(self):
+        return self.__name
+
+    # This method returns the value of the _ _animal_type field.
+    def get_animal_type(self):
+        return self.__animal_type
+
+    # This method returns the value of the _ _age field.
+    def get_age(self):
+        return self.__age
+
+    
