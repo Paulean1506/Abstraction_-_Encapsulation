5fe86f94523d6c934d4722c2b6843b888136cfaf
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 18:19:12 +0800

    create a test driver program for class car

--- Car/Car_Class.py
+++ Car/Car_Class.py
@@ -1,13 +1,31 @@
 # Paulean Marguerette F. Parrish
 # BSCPE 1-4
 # Write a class named Car that has the following data attributes:
+# _ _year_model (for the car’s year model)
+# _ _make (for the make of the car)
+# _ _speed (for the car’s current speed)
 class Car:
     def __init__(self, year_model, make):
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
 
-        # _ _year_model (for the car’s year model)
-        # _ _make (for the make of the car)
-        # _ _speed (for the car’s current speed)
+    #The class should also have the following methods:
+    # accelerate() The accelerate method should add 5 to the speed data attribute each time it is called.
+    def accelerate(self):
+        self.__speed += 5
+    
+    # brake() The brake method should subtract 5 from the speed data attribute each time it is called.
+    def brake(self):
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+    
+    # get_speed() The get_speed method should return the current speed.
+    def get_speed(self):
+        return self.__speed
+
+    
+
 
