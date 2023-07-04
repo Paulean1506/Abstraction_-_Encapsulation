6065c6996b0b0dab51865bd7e2183ec1f018d1fa
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 20:05:52 +0800

    fixed error

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -5,28 +5,33 @@
 from Pet_Class import Pet
 from termcolor import colored
 
+from termcolor import colored
+
 class TestPet:
- def display_visual(pet):
-    animal_type = pet.get_animal_type()
-    name = pet.get_name()
-    age = pet.get_age()
+    @staticmethod
+    def display_visual(pet):
+        animal_type = pet.get_animal_type()
+        name = pet.get_name()
+        age = pet.get_age()
 
-    print(colored("  \_/", "magenta"))
-    print(colored(" (^_^)", "magenta"))
-    print(colored(f" / {animal_type}", "magenta"))
-    print(colored(f"{name}", "cyan"))
-    print(colored(f"Age: {age} years", "yellow"))
+        print(colored("  \_/", "magenta"))
+        print(colored(" (^_^)", "magenta"))
+        print(colored(f" / {animal_type}", "magenta"))
+        print(colored(f"Name: {name}", "red"))
+        print(colored(f"Age: {age} years", "yellow"))
 
-def main():
-    pet = Pet()
+    @staticmethod
+    def main():
+        pet = Pet()
 
-    print(colored("Enter the details of your pet:", "cyan"))
-    pet.set_name(input("Name: "))
-    pet.set_animal_type(input("Animal Type: "))
-    pet.set_age(input("Age: "))
+        print(colored("\033[1;33m\nEnter the details of your pet:", "cyan"))
+        pet.set_name(input("\033[1;34mName: "))
+        pet.set_animal_type(input("\033[1;35mAnimal Type: "))
+        pet.set_age(input("\033[1;36mAge: "))
 
-    print(colored("\nPet Details:", "cyan"))
-     display_visual(pet)
+        print(colored("\033[1;37m\nPet Details:", "cyan"))
+        TestPet.display_visual(pet)
+
 
 if __name__ == "__main__":
-    main()
+    TestPet.main()
