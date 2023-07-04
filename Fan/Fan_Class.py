a384651a473283746224bc0d56b1cec6719e66c9
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 16:41:10 +0800

    added pseudocodes

--- Fan/Fan_Class.py
+++ Fan/Fan_Class.py
@@ -7,3 +7,10 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    # A private int data field named speed that specifies the speed of the fan.
+# A private bool data field named on that specifies whether the fan is on (the default is False).
+# A private float data field named radius that specifies the radius of the fan.
+# A private string data field named color that specifies the color of the fan.
+# The accessor(getters)  and mutator(setters)  methods for all four data fields.
+# A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
