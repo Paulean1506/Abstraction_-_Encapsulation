fe77b896997fac4cef9e76efe51987ff9333241e
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-04 19:17:05 +0800

    create folder for pet class

---
+++ Pet/Pet_Class.py
@@ -0,0 +1,6 @@
+# Paulean Marguerette F. Parrish
+# BSCPE 1-4
+# Write a class named Pet, which should have the following data attributes:
+# _ _name (for the name of a pet)
+# _ _animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
+# _ _age (for the pet’s age)
